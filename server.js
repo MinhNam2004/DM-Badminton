8b77cccea24a83b10c86602898551821110e54e1
MinhNam2004 <s3974896@rmit.edu.vn>
2026-04-22 10:12:45 +0700

    second commit

--- server.js
+++ server.js
@@ -1,21 +1,38 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 
 const app = express();
 
+// VIEW ENGINE
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// MIDDLEWARE
 app.use(express.static("public"));
+app.use("/uploads", express.static("uploads"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
 
-mongoose.connect("mongodb+srv://mina:123@dmbadminton.lpjcvef.mongodb.net/?appName=DMBadminton")
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+// CHECK ENV
+if (!process.env.MONGO_URI) {
+  console.log("❌ Thiếu MONGO_URI trong .env");
+  process.exit(1);
+}
 
+// CONNECT DB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch(err => console.log("❌ DB Error:", err));
+
+// ROUTES
 app.use("/", require("./routes/web"));
 
-app.listen(3000, () => console.log("http://localhost:3000"));
+// PORT
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("🚀 Server running on port " + PORT);
+});
